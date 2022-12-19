f84e88f31b259c0c60de789854b33faca37b45d2
Andrey K <andrey77865@proton.me>
2022-12-19 23:40:41 +0200

    final

--- decorators.py
+++ decorators.py
@@ -1,30 +1,18 @@
-import time
-
-
-def processing_time(func):
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        value = func(*args, **kwargs)
-        stop = time.perf_counter()
-        result_time = stop - start
-        print("Time: ", result_time)
-
-        return value
-
-    return wrapper
-
-
-def trace(func):
+def two_commands_func(func):
     def wrapper(*args):
         try:
             value = func(*args)
         except TypeError:
-            print(f"Give me name and phone separated by space")
-            exit()
-        except IndexError as error:
-            print(
-                f"Give me name and phone separated by space. You entered: {args}")
-            exit()
-        return value
+            print(f"Give me name and phone separated by space.")
+
+    return wrapper
+
+
+def one_command_func(func):
+    def wrapper(*args):
+        try:
+            value = func(*args)
+        except TypeError:
+            print(f"Give me name or phone")
 
     return wrapper
--- main.py
+++ main.py
@@ -1,57 +1,92 @@
-from decorators import trace
-
-supported_commands = ("hello", "add", "change", "phone", "show all")
-exit_words = ("good bye", "close", "exit")
-contacts = {}
+from decorators import two_commands_func, one_command_func
 
 
-@trace
+CONTACTS = {}
+
+
+@two_commands_func
 def add_contact(name, phone):
-    contacts[name] = phone
-    print(f"{name} {phone} has been added to contact list")
+    if not CONTACTS.get(name):
+        CONTACTS[name] = phone
+        print(f"'{name}' has been added to contact list")
+    else:
+        print(f"Contact '{name}' already exist")
 
 
-def show_all():
-    return contacts
+@two_commands_func
+def change_contact(name, phone):
+    if CONTACTS.get(name):
+        CONTACTS.update({name: phone})
+        print(f"Contact '{name}' has been changed")
+    else:
+        print(f"Contact '{name}' doesn't exist")
+
+
+@one_command_func
+def show_contact_phone(name):
+    get_phone = CONTACTS.get(name)
+    if get_phone:
+        print(f"{get_phone}")
+    else:
+        print(f"Contact '{name}' doesn't exist")
+
+
+def show_all(*args):
+    if len(CONTACTS) == 0:
+        print("Contact list is empty")
+    else:
+        for name, phone in CONTACTS.items():
+            print(f"Name: {name}, phone: {phone}\n")
 
 
 handler = {
     "hello": lambda: print("How can I help you?"),
     "add": add_contact,
-    "show all": show_all
+    "change": change_contact,
+    "phone": show_contact_phone,
+    "show": show_all
 }
 
 
-@trace
-def trace_command_to_handler(command):
-    # регулярка которая выдергивает команду
-    # получив команду, возвращаем команду отдельно и введенные данные строкой
-    # if command.startswith("add"):
-    splitted_command = command.split(" ")
-    command = splitted_command[0]
-
-    if command == "add":
-        name = splitted_command[1]
-        phone = splitted_command[2]
-        # print(f"{command}\n{name}\n{phone}\n")
-        return handler[command](name, phone)
-
-    # if command ==
+def commands_to_handler(main_command, user_input_list):
+    if len(user_input_list) == 0:
+        current_execution = handler[main_command]
+        return current_execution()
+    elif len(user_input_list) == 1:
+        current_execution = handler[main_command]
+        return current_execution(user_input_list[0])
+    elif len(user_input_list) == 2:
+        current_execution = handler[main_command]
+        return current_execution(user_input_list[0], user_input_list[1])
+    else:
+        print("Too many commands, try again.")
 
 
-while True:
-    user_input = input("Input you command: ").lower()
+def app():
+    while True:
+        user_input = input("Input command: ").lower()
 
-    if user_input in exit_words:
-        print("Good bye!")
-        break
-    elif user_input not in supported_commands:
-        print("Command doesn't supported, please try again")
-        continue
+        # STEP 1. Check if user want to exit of empty input
+        if user_input in ("good bye", "close", "exit"):
+            print("Good bye!")
+            break
+        elif len(user_input) == 0:
+            print("Empty input, please try again")
+            continue
 
-    splitted_command = trace_command_to_handler(user_input)
-    splitted_command = user_input.split(" ")
-    print(splitted_command)
-    current_execution = handler[splitted_command[0]]
-   # current_execution(splitted_command[1])
-   # print(contacts)
+        # STEP 2. Split user input to list and get the main command
+        user_input_list = user_input.split(" ")
+        main_command = user_input_list.pop(0)
+
+        # STEP 3. Check if main command is valid
+        if main_command not in ("hello", "add", "change", "phone", "show"):
+            print(
+                f"Unkmnown command '{main_command}'. You can use only 'hello', 'add', 'change', 'phone', 'show all'")
+            continue
+
+        # STEP 4. Execute
+        commands_to_handler(main_command, user_input_list)
+
+
+if __name__ == "__main__":
+    app()
